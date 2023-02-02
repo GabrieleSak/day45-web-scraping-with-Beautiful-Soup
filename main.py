48433769c045f3cb15263c6660fb20d20667bc8e
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-02-02 15:49:06 +0200

    Find particular elements with BeautifulSoup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+
 # import lxml
 
 with open('website.html', encoding='utf8') as f:
@@ -6,4 +7,28 @@ with open('website.html', encoding='utf8') as f:
 
 soup = BeautifulSoup(contents, 'html.parser')
 
-print(soup.title)
+# print(soup.title)
+# print(soup.title.string)
+#
+# print(soup.prettify())
+
+all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+# print(heading)
+
+
+section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get("class"))
+
+
+company_url = soup.select_one(selector="p a")
+# print(company_url)
+
+headings = soup.select(".heading")
+print(headings)
